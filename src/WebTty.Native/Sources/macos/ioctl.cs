6fe64ef335fff35fee959d3065be068922b5a7d5
Nick Van Dyck <vandyck.nick@outlook.com>
2019-07-08 22:57:10 +0200

    [feat] get default shell from current user
    - loads the default configured shell from current user on startup

--- src/WebTty.Native/Sources/macos/ioctl.cs
+++ src/WebTty.Native/Sources/macos/ioctl.cs
@@ -2,7 +2,7 @@ namespace WebTty.Native.Syscall
 {
     public static partial class Libc
     {
-        // https://github.com/torvalds/linux/blob/5bd4af34a09a381a0f8b1552684650698937e6b0/include/uapi/asm-generic/ioctls.h#L39
+        // https://github.com/apple/darwin-xnu/blob/0a798f6738bc1db01281fc08ae024145e84df927/bsd/sys/ttycom.h#L154
         public static long TIOCSWINSZ = 0x80087467;
     }
 }
