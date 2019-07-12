b5581dfeb1edd2b2c6e91c65b18076d754b5e0f8
Nick Van Dyck <vandyck.nick@outlook.com>
2019-07-12 21:28:52 +0200

    [feat] allow compilation on windows

---
+++ src/WebTty.Native/Sources/windows/libraries.cs
@@ -0,0 +1,8 @@
+namespace WebTty.Native
+{
+    internal static class Libraries
+    {
+        public const string libc = "libc.so.6";
+        public const string libutil = "libutil.so.1";
+    }
+}
