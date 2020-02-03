4efb0cc5db7fac6e4ccafa8c533cb7d12e5df66f
Nick Van Dyck <vandyck.nick@outlook.com>
2020-02-03 08:51:22 +0100

    chore: update dependencies

--- tools/build/Build.cs
+++ tools/build/Build.cs
@@ -1,6 +1,6 @@
-using System;
 using Nuke.Common;
 using Nuke.Common.Execution;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
@@ -8,7 +8,7 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
-using System.Runtime.InteropServices;
+using System;
 using System.Linq;
 
 [CheckBuildProjectConfigurations]
